5e2258a265ebd42745239a9e6bb148d48c61f8c6
HongYeseul <ghd8119@naver.com>
2024-09-26 19:00:34 +0900

    :tada: git init

---
+++ src/main/java/com/example/limited_product_shop/LimitedProductShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.limited_product_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitedProductShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitedProductShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/limited_product_shop/LimitedProductShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.limited_product_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitedProductShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
